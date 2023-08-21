b28d593867f4aa24bfbe3abc1fe55871d036b904
Srikanth Amaresi <37152336+Srikanth-ami@users.noreply.github.com>
2023-08-21 19:43:29 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,113 @@
+#this file consists of code for instances and sg
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIAQCJ5ZXYVC4WJODTI"
+secret_key = "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"
+}
+
+resource "aws_instance" "one" {
+  ami             = "ami-08e5424edfe926b43"
+  instance_type   = "t2.micro"
+  key_name        = "mykey"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1a"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+apt install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my app created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "web-server-1"
+  }
+}
+
+resource "aws_instance" "two" {
+  ami             = "ami-08e5424edfe926b43"
+  instance_type   = "t2.micro"
+  key_name        = "mykey"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1b"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+apt install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my website created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "web-server-2"
+  }
+}
+
+resource "aws_instance" "three" {
+  ami             = "ami-08e5424edfe926b43"
+  instance_type   = "t2.micro"
+  key_name        = "mykey"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "app-server-1"
+  }
+}
+
+resource "aws_instance" "four" {
+  ami             = "ami-08e5424edfe926b43"
+  instance_type   = "t2.micro"
+  key_name        = "mykey"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "app-server-2"
+  }
+}
+
+resource "aws_security_group" "five" {
+  name = "elb-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "six" {
+  bucket = "asdfghjklqwertyuiosdfghjkertyjk"
+}
+
+resource "aws_iam_user" "seven" {
+for_each = var.user_names
+name = each.value
+}
+
+variable "user_names" {
+description = "*"
+type = set(string)
+default = ["user1", "user2", "user3", "user4"]
+}
+
+resource "aws_ebs_volume" "eight" {
+ availability_zone = "ap-south-1c"
+  size = 30
+  tags = {
+    Name = "ebs"
+  }
+}
